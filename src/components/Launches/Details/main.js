ddbd037512593bad184ba3b4fa118e6cb4a0ec00
João Bicho <joaorbicho@gmail.com>
2021-02-25 17:42:11 +0000

    add: Adds a simple slider for the media elements

--- src/components/Launches/Details/main.js
+++ src/components/Launches/Details/main.js
@@ -3,66 +3,146 @@ import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
 const Article = styled.article`
+  margin: 0 auto;
   background-color: rgb(255, 255, 255);
   box-shadow: 0 10px 16px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  max-width: 800px;
+  min-width: 500px;
+  padding-bottom: 5px;
 
-  .text-content,
-  .footer {
-    text-align: center;
+  .text-content {
     padding: 20px;
   }
+`;
 
-  .footer {
-    .external-link {
-      padding: 10px;
-      border-radius: 5px;
+const Header = styled.header`
+  width: 100%;
+  position: relative;
+  height: 400px;
+
+  .slider {
+    position: relative;
+    height: inherit;
+    width: inherit;
+
+    .slider-wrapper {
+      position: relative;
+      height: inherit;
+      width: inherit;
+
+      .prev,
+      .next {
+        background: none;
+        border: none;
+        font-size: 45px;
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+        z-index: 1;
+        cursor: pointer;
+      }
+
+      .prev {
+        left: 20px;
+      }
+
+      .next {
+        right: 10px;
+      }
     }
   }
 `;
 
 const HeaderImage = styled.img`
-  width: 100%;
-  height: auto;
+  width: inherit;
+  position: absolute;
+  height: inherit;
 `;
 
-const Header = styled.header`
+const NoMediaText = styled.div`
   text-align: center;
 `;
 
 const Footer = styled.footer`
+  text-align: center;
+  padding: 20px;
+
   .external-link {
     text-decoration: none;
     color: inherit;
     border: solid rgb(237, 241, 245) 2px;
     border-radius: 5px;
+    padding: 10px;
   }
 `;
 
 function LaunchDetails() {
   let { id } = useParams();
   const [selectedMission, setSelectedMission] = useState(null);
+  const [activeImage, setActiveImage] = useState();
+  const [slideImages, setSlideImages] = useState([]);
 
   useEffect(() => {
     if (!selectedMission) {
       const missionList = JSON.parse(sessionStorage.getItem("missionList"));
-      const findMission = missionList[id];
+      const mission = missionList[id];
+      const missionImages = mission.ships.map((obj) => obj.image);
 
-      setSelectedMission(findMission);
+      setActiveImage(missionImages[0]);
+      setSlideImages(missionImages);
+      setSelectedMission(mission);
     }
   }, [selectedMission]);
+
+  const slideNav = (direction) => {
+    const imageIndex = slideImages.indexOf(activeImage);
+
+    if (
+      imageIndex + direction >= 0 &&
+      imageIndex + direction < slideImages.length
+    ) {
+      setActiveImage(slideImages[imageIndex + direction]);
+    }
+  };
 
   return selectedMission ? (
     <section>
       <Article>
         <Header>
-          {selectedMission.ships.length ? (
-            <HeaderImage
-              src={selectedMission.ships[0].image}
-              alt="launch media"
-            />
-          ) : (
-            <h2>No media available for this mission</h2>
-          )}
+          <div className="slider">
+            {slideImages.length ? (
+              <div className="slider-wrapper">
+                {slideImages.length > 1 && (
+                  <div>
+                    <button
+                      className="next"
+                      onKeyDown={() => slideNav(1)}
+                      onClick={() => slideNav(1)}
+                    >
+                      <span role="img" aria-label="arrow left">
+                        ➡️
+                      </span>
+                    </button>
+                    <button
+                      className="prev"
+                      onKeyDown={() => slideNav(-1)}
+                      onClick={() => slideNav(-1)}
+                    >
+                      <span role="img" aria-label="arrow right">
+                        ⬅️
+                      </span>
+                    </button>
+                  </div>
+                )}
+
+                <HeaderImage src={activeImage} alt="launch media" />
+              </div>
+            ) : (
+              <NoMediaText>
+                <h2>No media available for this mission</h2>
+              </NoMediaText>
+            )}
+          </div>
         </Header>
         <div className="text-content">
           <h2>{selectedMission.mission_name}</h2>
